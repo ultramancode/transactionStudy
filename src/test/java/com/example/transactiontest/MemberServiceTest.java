d5e4f09c3e318257f3e86045a92a91acc7c4aa43
taewoongKim <ktw2172@gmail.com>
2023-10-20 10:45:03 +0900

    feat: test code(@Transactional: service on, repository on(+recovery exception) fail

--- src/test/java/com/example/transactiontest/MemberServiceTest.java
+++ src/test/java/com/example/transactiontest/MemberServiceTest.java
@@ -1,9 +1,11 @@
 package com.example.transactiontest;
+import java.rmi.UnexpectedException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 
 @SpringBootTest
@@ -144,6 +146,33 @@ class MemberServiceTest {
         Assertions.assertTrue(logRepository.find(username).isEmpty());
 
     }
+
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: ON
+     * logRepository    @Transactional: ON Exception
+     */
+
+    // propagation.Required(기본 값)에선 LogRepository에서 exception 터뜨리고, 서비스에서 트라이 캐치로 잡아서 처리 한다고 해도
+    // 그냥 전체가 다 롤백되버린다.
+    // 내부 트랜잭션에서 예외 발생하면 rollbackOnly를 외부 트랜잭션에 마킹해버리니
+    // 트라이 캐치로 정상 흐름 처리해도 롤백 되버리는 것
+    @Test
+    void recoverException_fail() {
+
+        //given
+        String username = "로그예외_recoverException_fail";
+
+        //when
+        org.assertj.core.api.Assertions.assertThatThrownBy(
+            () -> memberService.joinV2(username)).isInstanceOf(UnexpectedRollbackException.class);
+
+        //then: 모든 데이터 롤백
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+
+    }
+
 
 
 
